d677cbeffbb16bf7828919a01be00127cdf4f7ae
Dawid <daw.tomczyk1997@gmail.com>
2023-01-30 12:29:20 +0100

    simple debug and final touches

--- src/app/rootsOfQuadraticEquation.js
+++ src/app/rootsOfQuadraticEquation.js
@@ -4,34 +4,28 @@ const rootsOfQuadraticEquation = (a, b, c) => {
   let root1 = null;
   let root2 = null;
 
-  if(typeof a !== "number" || typeof b !== "number" || typeof c !== "number") {
-    return "Możesz podawać tylko liczby"
-  }
-
   // Obliczanie delty
   let delta = b * b - 4 * a * c;
 
   // Sprawdzenie pierwiastków
   if (delta > 0) {
-    root1 = (-b + Math.sqrt(delta)) / (2 * a);
-    root2 = (-b - Math.sqrt(delta)) / (2 * a);
+    root1 = ((-b + Math.sqrt(delta)) / (2 * a)).toFixed(2);
+    root2 = ((-b - Math.sqrt(delta)) / (2 * a)).toFixed(2);
 
     console.log(
-      `Pierwiastki równania kwadratowego to ${root1.toFixed(
-        2
-      )} i ${root2.toFixed(2)}`
+      `Pierwiastki równania kwadratowego to ${root1} i ${root2}`
     );
-    return {
-      root1,
-      root2
-    };
+    return [
+      Number(root1),
+      Number(root2)
+    ];
   }
   // Kiedy delta jest równa 0
   else if (delta == 0) {
-    root1 = root2 = -b / (2 * a);
+    root1 = root2 = (-b / (2 * a)).toFixed(2);
 
     console.log(`Pierwiastek równania kwadratowego to ${root1}`);
-    return root1
+    return Number(root1);
   }
   // Brak pierwiastków
   else {
@@ -40,10 +34,15 @@ const rootsOfQuadraticEquation = (a, b, c) => {
 };
 
 // Liczby uzupełniane po włączeniu programu
-let a = Number(readline.question("Podaj wartosc a: "));
-let b = Number(readline.question("Podaj wartosc b: "));
-let c = Number(readline.question("Podaj wartosc c: "));
+const runProgram = () => {
+  let a = Number(readline.question("Podaj wartosc a: "));
+  let b = Number(readline.question("Podaj wartosc b: "));
+  let c = Number(readline.question("Podaj wartosc c: "));
+  
+  console.log(rootsOfQuadraticEquation(a, b, c));
+}
 
-console.log(rootsOfQuadraticEquation(a, b, c));
+// Aby uruchomić program manualnie - bez testów należy odkomentować poniższą linię i wpisać w terminalu npm start
+// runProgram()
 
 module.exports = rootsOfQuadraticEquation
